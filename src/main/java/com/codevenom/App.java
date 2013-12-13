c0b66640ec6f69d649853da5051cc707b8d6b305
David Der <david.der@gmail.com>
2013-12-13 12:37:22 -0500

    initial commit of maven project structure

---
+++ src/main/java/com/codevenom/App.java
@@ -0,0 +1,13 @@
+package com.codevenom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
